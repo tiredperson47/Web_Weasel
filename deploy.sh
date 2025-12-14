20049663ceb58549f2364d904ee1c96eba6fb845
tiredperson47 <tiredperson47@gmail.com>
2025-12-14 12:57:20 -0800

    Deploy fix + test data

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+/usr/bin/sudo /usr/bin/apt install docker.io docker-compose -y
+
 PASSDB=$(/usr/bin/tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32)
-# /usr/bin/sed -i "s/\(neo4j\/\).*/\1${PASSDB}/" ./neo4j_auth.txt
 echo "neo4j/$PASSDB" > ./neo4j_auth.txt
 
 /usr/bin/sudo /usr/bin/docker-compose up -d --build
 
-echo """
-===================================== DONE! =====================================
-
-To login to the Web Weasel web interface, open your browser and go to:
-http://localhost:8000
-
-Login with the following credentials:
-  Username: admin
-  Password: admin
-
-CHANGE THE DEFAULT PASSWORD IMMEDIATELY AFTER LOGGING IN!
-
-=================================================================================
-"""
+echo '===================================== DONE! ====================================='
+echo ''
+echo 'To login to the Web Weasel web interface, open your browser and go to:'
+echo 'http://localhost:8000'
+echo ''
+echo 'Login with the following credentials:'
+echo '  Username: admin'
+echo '  Password: admin'
+echo ''
+echo 'CHANGE THE DEFAULT PASSWORD IMMEDIATELY AFTER LOGGING IN!'
+echo ''
+echo '================================================================================='
